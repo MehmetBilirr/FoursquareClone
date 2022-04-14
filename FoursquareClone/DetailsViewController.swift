a2355ba46970e27b8d94f00ac763a99c54079b1b
Mehmet Bilir <mehmetbilir@aol.com>
2022-04-14 17:54:37 +0300

    mapkit  func defined

--- FoursquareClone/DetailsViewController.swift
+++ FoursquareClone/DetailsViewController.swift
@@ -8,20 +8,40 @@
 import UIKit
 import MapKit
 class DetailsViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
-
+    
+    var locationManager = CLLocationManager()
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var commentText: UITextField!
     @IBOutlet weak var placeTypeText: UITextField!
     @IBOutlet weak var placeNameText: UITextField!
     @IBOutlet weak var mapView: MKMapView!
+    var selectedName = ""
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        mapView.delegate = self
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
     }
+    
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if selectedName == "" {
+            
+        let location = CLLocationCoordinate2D(latitude: locations[0].coordinate.latitude, longitude: locations[0].coordinate.longitude)
+        let span = MKCoordinateSpan.init(latitudeDelta: 0.01, longitudeDelta: 0.01)
+        let region = MKCoordinateRegion(center: location, span: span)
+        mapView.setRegion(region, animated: true)
+        }
+    }
+    
     
     @IBAction func saveButtonClicked(_ sender: Any) {
     }
+    
+    
     
     
 }
