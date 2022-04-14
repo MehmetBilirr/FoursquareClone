7c6415ed14dec87391ed493af8334fba93774c22
Mehmet Bilir <mehmetbilir@aol.com>
2022-04-14 18:02:35 +0300

    get datas from firestore

--- FoursquareClone/ListViewController.swift
+++ FoursquareClone/ListViewController.swift
@@ -11,6 +11,8 @@ import Firebase
 class ListViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
     
     var selectedName = ""
+    var documentIdArray = [String]()
+    var placeNameArray = [String]()
     @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +22,8 @@ class ListViewController: UIViewController,UITableViewDelegate,UITableViewDataSo
         navigationController?.navigationBar.topItem?.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.add, target: self, action: #selector(addButtonClicked))
 
         navigationController?.navigationBar.topItem?.leftBarButtonItem = UIBarButtonItem(title: "Logout", style: UIBarButtonItem.Style.done, target: self, action: #selector(logOut))
+        
+        getDataFromFirestore()
     }
     
     @objc func logOut(){
@@ -38,6 +42,65 @@ class ListViewController: UIViewController,UITableViewDelegate,UITableViewDataSo
         
         performSegue(withIdentifier: "toDetailsVC", sender: nil)
         
+    }
+    
+    func getDataFromFirestore(){
+        
+        let firestoreData = Firestore.firestore()
+        firestoreData.collection("Places").addSnapshotListener { snapshot, error in
+            if error != nil {
+                self.alert(titleId: "Error!", messageId: error?.localizedDescription ?? "Error!")
+            }else {
+                if snapshot?.isEmpty != true && snapshot != nil {
+                    
+                    
+                    self.documentIdArray.removeAll(keepingCapacity: false)
+                    self.placeNameArray.removeAll(keepingCapacity: false)
+                    
+                    for document in snapshot!.documents {
+                        
+                        let documentId = document.documentID
+                        
+                        self.documentIdArray.append(documentId)
+                        
+                        
+                        
+                        if let placeName = document.get("name") as? String {
+                            self.placeNameArray.append(placeName)
+                           
+                        }
+                    }
+                    self.tableView.reloadData()
+                    
+                    
+                }
+                
+                
+                
+                
+                
+            }
+        }
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    func alert(titleId:String,messageId:String){
+        let alert = UIAlertController(title: titleId, message: messageId, preferredStyle: UIAlertController.Style.alert)
+        let okButton = UIAlertAction(title: "OK", style: UIAlertAction.Style.cancel)
+        alert.addAction(okButton)
+        self.present(alert, animated: true)
     }
     
 
