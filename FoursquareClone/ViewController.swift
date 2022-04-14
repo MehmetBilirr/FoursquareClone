2d77c6c3ce1c720d5714b0b271a9d895bae82971
Mehmet Bilir <mehmetbilir@aol.com>
2022-04-14 17:47:43 +0300

    signup and signin funcs

--- FoursquareClone/ViewController.swift
+++ FoursquareClone/ViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Firebase
 
 class ViewController: UIViewController {
     @IBOutlet weak var emailText: UITextField!
@@ -13,12 +14,62 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+       
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        let currentUser = Auth.auth().currentUser
+        if  currentUser != nil {
+            
+            performSegue(withIdentifier: "toListVC", sender: nil)
+        }
+    }
+    
     @IBAction func signInClicked(_ sender: Any) {
+        
+        if emailText.text != "" && passwordText.text != "" {
+            let auth = Auth.auth()
+            auth.signIn(withEmail: emailText.text!, password: passwordText.text!) { data, error in
+                if error != nil {
+                    self.alert(titleId: "Error", messageId: error?.localizedDescription ?? "Error")
+                    
+                    
+                }else {
+                    self.performSegue(withIdentifier: "toListVC", sender: nil)
+                }
+            }
+            
+        }
+        
     }
     
     @IBAction func signOutClicked(_ sender: Any) {
+        
+        if emailText.text != "" && passwordText.text != "" {
+            
+            let auth = Auth.auth()
+            auth.createUser(withEmail: emailText.text!, password: passwordText.text!) { data, error in
+                if error != nil {
+                    self.alert(titleId: "Error!", messageId: error?.localizedDescription ?? "Error!")
+                    
+                    
+                }else {
+                    self.performSegue(withIdentifier: "toListVC", sender: nil)
+                }
+            }
+            
+        }else {
+            alert(titleId: "Error!", messageId: "E-mail/Password empty.")
+            
+        }
+    }
+    
+    func alert(titleId:String,messageId:String){
+        let alert = UIAlertController(title: titleId, message: messageId, preferredStyle: UIAlertController.Style.alert)
+        let okButton = UIAlertAction(title: "OK", style: UIAlertAction.Style.cancel)
+        alert.addAction(okButton)
+        self.present(alert, animated: true)
     }
     
 }
